bd156706febd5e5904a4f735ac1c73f58249775a
AmnaHassan1 <amnahassan622@yahoo.com>
2023-11-02 03:46:23 -0600

    OrderAlert

---
+++ ordering-system/src/OrderAlert.js
@@ -0,0 +1,37 @@
+// OrderAlert.js
+import { useState } from "react";
+import "./OrderAlert.css";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Navbar from "./Navbar"; // Import the Navbar component
+
+function OrderAlert({ item, onClose }) {
+  return (
+    <div className="order-alert">
+      <Navbar /> {/* Include the Navbar component here */}
+      <div className="spacer"></div>
+      <div className="order-alert-content">
+        <div className="border-box">
+          <div className="disclaimer">
+            <p className="red-text">Order Alert Disclaimer</p>
+          </div>
+          <div className="box">
+            <p>The item "{item}" is unavailable</p>
+          </div>
+          <div className="box">
+            <p>Click here to make a new selection</p>
+          </div>
+          <div className="box">
+          <p>We are sorry for any inconvenience 😞</p>
+            {/* Use the HTML entity &#128532; for a sad face */}
+          </div>
+          <button onClick={onClose} className="close-button">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OrderAlert;
--- ordering-system/src/index.js
+++ ordering-system/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Menu from "../src/Menu Pages/Menu";
+import OrderAlert from "./OrderAlert";
 
 import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -9,7 +10,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Menu />
+    <OrderAlert />
   </React.StrictMode>
 );
 
