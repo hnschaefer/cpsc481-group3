d7e4a7fe95bbb5f71919616a4d7f19d5e0288788
AnindaS13 <73033758+AnindaS13@users.noreply.github.com>
2023-12-01 19:03:57 -0700

    alert done

--- ordering-system/src/OrderAlert.js
+++ ordering-system/src/OrderAlert.js
@@ -1,31 +1,28 @@
 // OrderAlert.js
 import "./OrderAlert.css";
-import Navbar from "./Navbar"; // Import the Navbar component
+import ReturnToMenuNavbar from "./ReturnToMenuNavbar";
 
-function OrderAlert({ item, onClose }) {
+function OrderAlert() {
   return (
-    <div className="order-alert">
-      <Navbar /> {/* Include the Navbar component here */}
-      <div className="spacer"></div>
-      <div className="order-alert-content">
-        <div className="border-box">
-          <div className="disclaimer">
-            <p className="red-text">Order Alert Disclaimer</p>
-          </div>
-          <div className="box">
-            <p>The item "{item}" is unavailable</p>
-          </div>
-          <div className="box">
-            <p>Click here to make a new selection</p>
-          </div>
-          <div className="box">
-            <p>We are sorry for any inconvenience 😞</p>
-            {/* Use the HTML entity &#128532; for a sad face */}
-          </div>
-          <button onClick={onClose} className="close-button">
-            Close
-          </button>
-        </div>
+    <div>
+      <div
+        style={{
+          backgroundColor: "#414042",
+          paddingTop: 20,
+          paddingBottom: 20,
+        }}
+      >
+        <ReturnToMenuNavbar />
+      </div>
+      <div className="alert-container">
+        <h1 className="disclaimer">Order Alert Disclaimer</h1>
+        <p className="text">This item is unavailable.</p>
+        <p className="texttwo">We are sorry for any inconvenience. 😞</p>
+        <p className="textthree">
+          You may press the close button below to go back to the order cart or
+          select the order cart button above.
+        </p>
+        <button className="close">Close</button>
       </div>
     </div>
   );
